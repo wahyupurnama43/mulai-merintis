f2a0b2f08b94e435599fa84924e48450e463c2bd
wahyupurnama43 <wahyupurnama434@gmail.com>
2021-07-21 17:37:06 +0800

    first upload

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+$('li.dropdown > a').on('click', function (event) {
+    const dropdown = document.querySelector("ul li.dropdown");
+    event.preventDefault();
+    $(this).toggleClass('selected');
+    $(this).parent().find('ul').first().toggle(300);
+    $(this).parent().siblings().find('ul').hide(200);
+
+    //Hide menu when clicked outside
+    $(this).parent().find('ul').parent().mouseleave(function () {
+        var thisUI = $(this);
+        $('html').click(function () {
+            thisUI.children(".dropdown-menu").hide();
+            $('html').unbind('click');
+        });
+    });
+    dropdown.classList.toggle("mt-dropdown");
+});
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav ul#nav");
+menuToggle.addEventListener("click", function () {
+    nav.classList.toggle("slidess");
+});
+
+$(document).scroll(function () {
+    var y = $(window).scrollTop();
+    if (y > 0) {
+        $("nav.menu").addClass("shActive");
+    } else {
+        $("nav.menu").removeClass("shActive");
+    }
+
+});
